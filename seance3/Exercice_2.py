86f4f7823882284ea29482b659ff684ed498761c
LaCwisss <christian.nicholson@gmail.com>
2024-03-27 11:48:03 +0100

    fin de seance, jusqu'a __str__

--- seance3/Exercice_2.py
+++ seance3/Exercice_2.py
@@ -20,11 +20,31 @@ class Tree:
         return count
     
     def child(self, i: int):
+        assert i <= len(self.children())
         return self.children()[i]
     
     def is_leaf(self):
         return self.nb_children1()==0 
-        
+    
+    def depth(self):
+        depth = 0 
+        if len(self.children())>0:
+            depth = 1
+        for child in range(len(self.children())):
+            depth = depth + self.__children[child].depth()
+        return depth
+    
+    def __str__(self):
+        prefix = f'{self.__label}'
+        if len(self.children())==0:
+            return prefix
+        prefix = prefix +'('
+        for child in range(len(self.children())):
+            prefix = prefix + f'{self.children()[child].__str__()},'
+        prefix = prefix[:-1] +')'
+        return prefix
+    
+
 if __name__=='__main__':
     t1=Tree('a')
     print(t1.label())
@@ -35,4 +55,7 @@ if __name__=='__main__':
     print(t3.nb_children1())
     print(t3.nb_children2())
     print(t3.child(1))
-    print(t1.is_leaf())
+    print(t3.is_leaf())
+    print(t3.depth())
+    print(t3.__str__())
+    
