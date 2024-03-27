86e6548cac3956fff1c84988f9a99758aa90b224
LaCwisss <christian.nicholson@gmail.com>
2024-03-27 11:04:51 +0100

    nb_children

---
+++ seance3/Exercice_2.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+class Tree:
+    def __init__(self, label, *children):
+        self.__label = label
+        self.__children = children
+    
+    def label(self):
+        return f"{self.__label}"
+    
+    def children(self):
+        return self.__children
+    
+    def nb_children1(self):
+        return len(self.__children)
+    def nb_children2(self):
+        count=len(self.__children)
+        for child in range(len(self.__children)):
+            count = count + self.children()[child].nb_children2()           
+        return count
+    
+    def child(self, i: int):
+        return 
+        
+if __name__=='__main__':
+    t1=Tree('a')
+    print(t1.label())
+    t2 = Tree('a',Tree('b'),Tree('c'))
+    print(t2.children())
+    print(t2.nb_children1())
+    t3 = Tree('a',Tree('b'),Tree('c',Tree('d')))
+    print(t3.nb_children1())
+    print(t3.nb_children2())
+    
